9ebcf7d727ebd83db2c0e63972ff6f2400eb4d7d
Chu Ngọc Thắng <chuthanglsz@gmail.com>
2024-11-14 17:58:22 +0700

    Update security

--- src/main/java/com/javaweb/utils/MessageUtils.java
+++ src/main/java/com/javaweb/utils/MessageUtils.java
@@ -3,7 +3,7 @@ package com.javaweb.utils;
 public class MessageUtils {
     public static final Long USER_ID = 4L;
     public static final String[] PUBLIC_URLS = {"/","/assets/**","/register","/login","/auth/**"};
-    public static final String[] USER_URLS = { "/api/user/**","/api/wallet/**" };
+    public static final String[] USER_URLS = { "/api/user/**","/api/wallet/**"};
     public static final String[] ADMIN_URLS = { "/api/admin/**" };
     public static final String DEFAULT_PASSWORD = "123456";
 }
