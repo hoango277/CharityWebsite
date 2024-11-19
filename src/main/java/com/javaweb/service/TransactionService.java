95878ad7f5e173e6f0c7058c34224444bde0967a
Hòa Ngô <xuanhoa2772004@gmail.com>
2024-11-19 13:09:52 +0700

    ADD transcatin controller

--- src/main/java/com/javaweb/service/TransactionService.java
+++ src/main/java/com/javaweb/service/TransactionService.java
@@ -1,4 +1,11 @@
 package com.javaweb.service;
 
+import com.javaweb.model.dto.ResponseDTO;
+import com.javaweb.model.response.TransactionResponse;
+
+import java.text.ParseException;
+import java.util.List;
+
 public interface TransactionService {
+    ResponseDTO getAllTransactions() throws ParseException;
 }
--- src/main/java/com/javaweb/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/javaweb/service/impl/TransactionServiceImpl.java
@@ -1,12 +1,36 @@
 package com.javaweb.service.impl;
 
+import com.javaweb.converter.TransactionConverter;
+import com.javaweb.entity.TransactionEntity;
+import com.javaweb.model.dto.ResponseDTO;
+import com.javaweb.model.response.TransactionResponse;
 import com.javaweb.repository.TransactionRepository;
 import com.javaweb.service.TransactionService;
+import jakarta.transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
+    @Autowired
+    private TransactionConverter transactionConverter;
+
+
+    @Override
+    public ResponseDTO getAllTransactions() throws ParseException {
+        List<TransactionEntity> transactions = transactionRepository.findAll();
+        List<TransactionResponse> transactionResponses = new ArrayList<>();
+        for (TransactionEntity transaction : transactions) {
+            TransactionResponse transactionResponse = transactionConverter.convertToResponse(transaction);
+            transactionResponses.add(transactionResponse);
+        }
+        return ResponseDTO.builder().data(transactionResponses).message("Lấy tất cả transaction thành công")
+                .detail("OK").build();
+    }
 }
